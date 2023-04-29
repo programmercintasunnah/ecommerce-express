87318d1d2000453462efa3eeb7b5173cde345920
programmercintasunnah <11751100101@students.uin-suska.ac.id>
2023-04-29 14:24:19 +0700

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { sequelize } = require("./models");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 const app = express();
 
-const port = 8000;
+dotenv.config();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+const publicPathDirectory = path.join(__dirname, "public");
+app.use(express.static(publicPathDirectory));
+
+app.use(cookieParser());
+app.use(express.json());
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to the database", err);
+  });
+
+const port = process.env.PORT || 9000;
 
 app.listen(port, async () => {
   console.log(`server berjalan di port ${port}`);
