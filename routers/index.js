bcf9165faac6af8d190f37723ff1f31171d63789
programmercintasunnah <11751100101@students.uin-suska.ac.id>
2023-05-01 11:24:48 +0700

    add comment & rating

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cartRouter = require("./carts");
+const commentRouter = require("./comments");
 const productRouter = require("./products");
 const userRouter = require("./users");
 
@@ -8,5 +9,6 @@ const rootRouter = express.Router();
 rootRouter.use("/users", userRouter);
 rootRouter.use("/products", productRouter);
 rootRouter.use("/carts", cartRouter);
+rootRouter.use("/comments", commentRouter);
 
 module.exports = rootRouter;
